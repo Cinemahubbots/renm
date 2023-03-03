e222e20cc929d98ef0d092b5a911a79542b76b46
railwaydevpack <125719616+railwaydevpack@users.noreply.github.com>
2023-03-03 15:41:39 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -19,9 +19,9 @@ async def start_cmd(bot, msg):
 
 @Client.on_callback_query(filters.regex("start"))
 async def start(bot, msg, cb=True):   
-    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://github.com/MrMKN>MrMKN</a></b>"                                     
+    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage"                                     
     button= [[
-        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/mkn_bots_updates")
+        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/beta_botz")
         ],[
         InlineKeyboardButton("ℹ️ Help", callback_data="help"),
         InlineKeyboardButton("📡 About", callback_data="about") 
