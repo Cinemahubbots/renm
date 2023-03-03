53927467ad67d480c0b8845bee905e85c0dccb7b
railwaydevpack <125719616+railwaydevpack@users.noreply.github.com>
2023-03-03 18:05:33 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -44,45 +44,21 @@ async def rename_file(bot, msg):
         og_thumbnail = await bot.download_media(og_media.thumbs[0].file_id)
     await sts.edit("Trying to Uploading")
     c_time = time.time()
-    try:
-        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
-    except Exception as e:  
-        await sts.edit(f"Error {e}") 
-        return               
-    try:
-        os.remove(downloaded)
-        os.remove(og_thumbnail)
-    except:
-        pass
-    #await sts.delete()
-     
     value = 2090000000
     if value < file.file_size:
-        await sts.edit("```Trying To Upload```")
         try:
-            filw = await app.send_document(log_channel,document = file_path,thumb=ph_path,caption = caption,progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))
-            from_chat = filw.chat.id
-            mg_id = filw.id
-            time.sleep(2)
-            await bot.copy_message(msg.from_user.id,from_chat,mg_id)
-            await sts.delete()
-            os.remove(file_path)
-            try:
-                os.remove(ph_path)
-            except:
-                pass
-        except Exception as e:
-            await sts.edit(e)
-            os.remove(file_path)
-            try:
-                os.remove(ph_path)
-            except:
-                return
-    else:
-    	   await sts.edit("```Trying To Upload```")
-    	   c_time = time.time()
-     	   await bot.send_document(msg.from_user.id,document = file_path,thumb=ph_path,caption = caption,progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))			
-     	   await sts.delete()
-     	   os.remove(file_path)
-
-     			     	
+            await app.send_document(log_channel, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+        except Exception as e:  
+            await sts.edit(f"Error {e}") 
+            return               
+        try:
+            os.remove(downloaded)
+            os.remove(og_thumbnail)
+        except:
+            pass
+        await sts.delete()
+    else:  
+        try:
+            await app.send_document(log_channel, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+     
+   
