70d9aaa488b9975b54ef663bf13248aef4d12e45
railwaydevpack <125719616+railwaydevpack@users.noreply.github.com>
2023-03-04 12:11:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,23 +10,10 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=100,
+            workers=200,
             plugins={"root": "main"},
             sleep_threshold=10,
         )
-    async def start(self):
-        await super().start()
-        me = await self.get_me()   
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        port = "8080"
-        await web.TCPSite(app, bind_address, port).start()   
-        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
-       
-    async def stop(self, *args):
-       await super().stop()      
-       print("Bot Restarting........")
 
 
 bot = Bot()
